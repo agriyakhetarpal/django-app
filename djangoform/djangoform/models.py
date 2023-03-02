2d279d628c1bb1e4bede3ca989ffb3568ea47789
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-03-02 17:34:19 +0530

    debug and finalise form

--- djangoform/djangoform/models.py
+++ djangoform/djangoform/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+# declare explicit app label name
+app_label = 'djangoform'
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
- # declare explicit app label name
-app_label = 'djangoform'
+    email = models.EmailField(max_length=100)
