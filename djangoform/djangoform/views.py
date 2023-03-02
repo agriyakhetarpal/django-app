cfc0fe47cf8bebd88b9a7e6103fa0915869198ec
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2023-03-02 16:58:05 +0530

    add views and templates for form

--- djangoform/djangoform/views.py
+++ djangoform/djangoform/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from myapp.forms import ContactForm
 
+# display and process the form after validating the data
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact.html', {'form': form})
 
-def welcome(request):
-	return HttpResponse("Welcome to the form page")
+ # view to display the success page
+def success(request):
+    return render(request, 'success.html')
 
